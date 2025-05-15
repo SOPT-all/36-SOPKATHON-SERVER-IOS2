587fe597249ef622b5d03ef8e37e10c1dcf6149a
kaswhy <sxshx818@naver.com>
2025-05-16 01:42:12 +0900

    [FEAT] 프로젝트 초기 세팅

---
+++ src/main/java/org/sopt36th/sopthackathon/SoptHackathonApplication.java
@@ -0,0 +1,13 @@
+package org.sopt36th.sopthackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoptHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt36th/sopthackathon/SoptHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt36th.sopthackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoptHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
