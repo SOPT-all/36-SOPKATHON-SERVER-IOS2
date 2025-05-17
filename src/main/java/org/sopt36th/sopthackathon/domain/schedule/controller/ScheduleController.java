b4520fee86db06ffa608abee874c1a9aef0726da
kaswhy <sxshx818@naver.com>
2025-05-18 02:47:16 +0900

    [FEAT] Schedule 관련 스켈레톤 코드 추가

---
+++ src/main/java/org/sopt36th/sopthackathon/domain/schedule/controller/ScheduleController.java
@@ -0,0 +1,18 @@
+package org.sopt36th.sopthackathon.domain.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt36th.sopthackathon.domain.schedule.service.ScheduleService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ScheduleController {
+
+    private ScheduleService scheduleService;
+
+    // 소연
+
+
+    // 승준
+
+}
---
+++ src/main/java/org/sopt36th/sopthackathon/domain/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,14 @@
+package org.sopt36th.sopthackathon.domain.schedule.repository;
+
+import org.sopt36th.sopthackathon.domain.schedule.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    // 소연
+
+
+    // 승준
+
+}
