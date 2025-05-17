576b611ab0356d399c345a1dbda04211ece313d9
kaswhy <sxshx818@naver.com>
2025-05-18 03:04:17 +0900

    [FEAT] 모든 컨트롤러 RequestMapping 추가

--- src/main/java/org/sopt36th/sopthackathon/domain/schedule/controller/ScheduleController.java
+++ src/main/java/org/sopt36th/sopthackathon/domain/schedule/controller/ScheduleController.java
@@ -1,11 +1,15 @@
 package org.sopt36th.sopthackathon.domain.schedule.controller;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.sopt36th.sopthackathon.domain.schedule.service.ScheduleService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class ScheduleController {
 
     private ScheduleService scheduleService;
