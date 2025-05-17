9628e5ea2530cf875ce5e542b715251ade3a292a
kaswhy <sxshx818@naver.com>
2025-05-18 03:59:38 +0900

    [FEAT] #3: 전체 수업 조회

--- src/main/java/org/sopt36th/sopthackathon/domain/course/domain/Course.java
+++ src/main/java/org/sopt36th/sopthackathon/domain/course/domain/Course.java
@@ -1,8 +1,31 @@
 package org.sopt36th.sopthackathon.domain.course.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.sopt36th.sopthackathon.global.entity.BaseEntity;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@RequiredArgsConstructor
 @Entity
 public class Course extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "shop_id")
+    private final Shop shop;
+
+    private final String productName;
+
+    private final String title;
+
+    private final String description;
+
+    private final String ingredient;
+
+    private final String image;
 }
--- src/main/java/org/sopt36th/sopthackathon/domain/course/service/CourseService.java
+++ src/main/java/org/sopt36th/sopthackathon/domain/course/service/CourseService.java
@@ -1,9 +1,15 @@
 package org.sopt36th.sopthackathon.domain.course.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.sopt36th.sopthackathon.domain.course.domain.Course;
+import org.sopt36th.sopthackathon.domain.course.dto.response.GetCourseResponse;
+import org.sopt36th.sopthackathon.domain.course.dto.response.GetCoursesResponse;
 import org.sopt36th.sopthackathon.domain.course.repository.CategoryRepository;
 import org.sopt36th.sopthackathon.domain.course.repository.CourseRepository;
 import org.sopt36th.sopthackathon.domain.course.repository.ShopRepository;
+import org.sopt36th.sopthackathon.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,9 +19,24 @@ public class CourseService {
     private final CourseRepository courseRepository;
     private final CategoryRepository categoryRepository;
     private final ShopRepository shopRepository;
+    private final UserRepository userRepository;
 
     // 소연
+    public GetCoursesResponse getAllCourses(String phoneNumber) {
+        List<Course> courses = courseRepository.findAll();
 
+        List<GetCourseResponse> courseResponses = courses.stream()
+                .map(course -> new GetCourseResponse(
+                        course.getId(),
+                        course.getImage(),
+                        course.getProductName(),
+                        course.getDescription(),
+                        course.getIngredient()
+                ))
+                .collect(Collectors.toList());
+
+        return new GetCoursesResponse(courseResponses);
+    }
 
     // 승준
 
