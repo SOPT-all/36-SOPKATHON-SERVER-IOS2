5de961245ea376f3fb1ca46372c8413922301188
lezgititt <155799025+lezgititt@users.noreply.github.com>
2025-05-18 05:28:23 +0900

    [Feat] #5: 수업 상세 조회

--- src/main/java/org/sopt36th/sopthackathon/domain/schedule/domain/Schedule.java
+++ src/main/java/org/sopt36th/sopthackathon/domain/schedule/domain/Schedule.java
@@ -1,8 +1,22 @@
 package org.sopt36th.sopthackathon.domain.schedule.domain;
 
+import jakarta.persistence.Access;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.sopt36th.sopthackathon.global.entity.BaseEntity;
 
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@RequiredArgsConstructor
 @Entity
 public class Schedule extends BaseEntity {
+    private final Long courseId;
+    private final Long userId;
+    private final LocalDateTime time;
+    private final int numberOfPeople;
 }
